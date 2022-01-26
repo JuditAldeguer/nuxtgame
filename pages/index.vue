cccfb1d4877f8f5786d89d6446754df6057af2b4
juditAldeguer <jud.vicens@hotmail>
2022-01-26 18:42:51 +0100

    feat: HTML initial structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <Tutorial />
+<div class="flex bg-red-400 intem-center justify-center h-screen flex-col relative">
+  <div class="tablero grid grid-cols-3">
+    <div v-for="(item, i) in 9" :key="i" class="bg-black p-4 text-white">0</div>
+  </div>
+</div>
 </template>
 
 <script>
