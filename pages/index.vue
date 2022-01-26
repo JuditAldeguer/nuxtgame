ad9ef358785f41637d7df5ca4862b94f1ebfa063
juditAldeguer <jud.vicens@hotmail>
2022-01-26 18:46:25 +0100

    build: tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="flex bg-red-400 intem-center justify-center h-screen flex-col relative">
   <div class="tablero grid grid-cols-3">
-    <div v-for="(item, i) in 9" :key="i" class="bg-black p-4 text-white">0</div>
+    <div v-for="(item, i) in 9" :key="i" class="bg-black p-4 text-white h-[10px]">0</div>
   </div>
 </div>
 </template>
